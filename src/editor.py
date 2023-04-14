49b106bf9e525a2997c890439d36d7ba9e0c299f
Tasi Dzikiti <joedziks@gmail.com>
2023-04-14 06:52:42 -0400

    thumbnail,gif and video with text app
    Run python editor.py inside the src folder in your terminal. Import audio and Video and type in text to show on the video. Click process and choose output folder.

--- src/editor.py
+++ src/editor.py
@@ -9,7 +9,7 @@ import string
 
 from moviepy.config import change_settings
 change_settings(
-    {"IMAGEMAGIC_BINARY":  r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"})
+    {"IMAGEMAGICK_BINARY":  r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"})
 
 
 # Creating the interface
@@ -55,10 +55,69 @@ class VideoEditorGUI(tk.Frame):
     def open_dialog(self):
         self.master.filename = filedialog.askopenfilename(
             initialdir="/", title="Select your files", filetypes=[('All files', '*.*')])
-        print(self.master.filename)
+        # Types of files to be output
+        if mimetypes.guess_type(self.master.filename)[0].startswith('video'):
+            openFileLabel = tk.Label(self.master, anchor="e", justify=tk.LEFT, text="Location: " +
+                                     self.master.filename + " Type: " + "Video", font=("Helvatica", 13))
+            openFileLabel.grid(sticky="W", column=0, row=len(
+                self.importedObjects) + 4, padx=10, pady=2)
+            file = FileObject(self.master.filename, openFileLabel, "video")
+            self.importedObjects.append(file)
+        else:
+            openFileLabel = tk.Label(self.master, anchor="e", justify=tk.LEFT, text="Location: " +
+                                     self.master.filename + " Type: " + "Audio", font=("Helvatica", 13))
+            openFileLabel.grid(sticky="W", column=0, row=len(
+                self.importedObjects) + 4, padx=10, pady=2)
+            file = FileObject(self.master.filename, openFileLabel, "audio")
+            self.importedObjects.append(file)
+
+    def get_random_string(self, length):
+        letters = string.ascii_lowercase
+        result_str = ''. join(random.choice(letters) for i in range(length))
+        return result_str
 
     def processData(self):
-        print("Processing")
+        dir_name = filedialog.askdirectory()
+
+        file_name_extension = "/" + self.get_random_string(8)
+        video_name = dir_name + file_name_extension + "_video" + ".mp4"
+        gif_name = dir_name + file_name_extension + "_gif" + ".gif"
+        thumbnail_name = dir_name + file_name_extension + "_thumbnail" + ".jpeg"
+
+        video_clip_found = None
+        audio_clip_found = None
+        for file in self.importedObjects:
+            if (file.data_type == "video"):
+                video_clip_found = file
+            elif (file.data_type == "audio"):
+                audio_clip_found = file
+
+        if (video_clip_found and audio_clip_found):
+            clip = VideoFileClip(video_clip_found.path)
+
+            overlayEntry = self.widgets
+            # Text you will put on video (Adjust to set duration)
+            clip_overlay = TextClip(overlayEntry.get(), color="blue", font="Roboto",
+                                    kerning=5, fontsize=150).set_position("center").set_duration(15)
+
+        frame = clip.get_frame(int(10))
+        thumb_image = Image.fromarray(frame)
+        thumb_image.save(thumbnail_name)
+
+        # Background music.
+        background_audio_clip = AudioFileClip(audio_clip_found.path)
+        # Note here that the music plays to the clip duration
+        bg_music = background_audio_clip.subclip(0, clip.duration)
+
+        # Compression
+        resized_clip = clip.resize(0.5)
+        resized_clip.write_gif(gif_name)
+        # Alternative: ffmpeg -i input.mp4 -vcodec h264 -acodac aad output.mp4
+
+        clip_with_audio = clip.set_audio(bg_music)
+        final_clip = CompositeVideoClip([clip_with_audio, clip_overlay])
+        final_clip.write_videofile(
+            video_name, codec='libx264', audio_codec='aac')
 
 
 # Root frame
